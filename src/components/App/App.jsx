83c24ebaa0785389015fa54ee03cfd3a111d50c1
Ryan Zomparelli <zomp.ryan@gmail.com>
2025-08-10 14:53:15 -0400

    feat(routing): add routes for Reviews and AboutMe components

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -2,6 +2,8 @@ import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import Header from "../Header/Header";
 import Dashboard from "../Dashboard/Dashboard";
+import Reviews from "../Reviews/Reviews";
+import AboutMe from "../AboutMe/AboutMe";
 
 function App() {
   return (
@@ -9,6 +11,8 @@ function App() {
       <Header />
       <Routes>
         <Route path="/" element={<Dashboard />} />
+        <Route path="/Reviews" element={<Reviews />} />
+        <Route path="/AboutMe" element={<AboutMe />} />
       </Routes>
     </div>
   );
