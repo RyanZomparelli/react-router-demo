db2e38c991af4024c17f809f3aebdb67894dcd52
Ryan Zomparelli <zomp.ryan@gmail.com>
2025-08-11 08:13:20 -0400

    chore(NavBar): add explanatory comment

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -2,6 +2,7 @@ import { NavLink } from "react-router-dom";
 import "./NavBar.css";
 
 function NavBar() {
+  //NavLink takes a special isActive object to aid in styling
   function addCustomClassName({ isActive }) {
     return "menu__link" + (isActive ? " menu__link_active" : "");
   }
