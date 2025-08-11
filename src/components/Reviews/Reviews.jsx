25476e3e05972407446b2b678b03c914cc7663ff
Ryan Zomparelli <zomp.ryan@gmail.com>
2025-08-11 15:54:43 -0400

    feat(Reviews): fetch review data from the server and dynamically create routes to each review
    - Using the useEffect hook and an empty array dependency fetch the reviews once upon mounting.
    - Using the data from the API set the inital reviews state using the setter function from the useState hook.
    - Create a Review component and pass the state to the Reviews and Review components as a prop.
    - Dynamically render a list of links using the map method on the reviews state in the Reviews component.
    - Add a new Route in App.jsx that will dynamically navigate to the link that is clicked using the useParams hook.
    - Add styles to Review and Reviews components.

--- src/components/Reviews/Reviews.jsx
+++ src/components/Reviews/Reviews.jsx
@@ -1,7 +1,23 @@
-function Reviews() {
+import { Link } from "react-router-dom";
+import "./Reviews.css";
+
+function Reviews({ reviews }) {
+  //Here, we're creating a list of review links from the reviews state. When you
+  //click on a link, the URL will reflect the id of the review.
+  //The 'to' attribute must take a string. So to use the id number from the Api
+  //  in the Link component we use a template literal.
   return (
-    <div>
-      <p>My reviews will go here!</p>
+    <div className="reviews">
+      <ul className="reviews__list">
+        {reviews &&
+          reviews.map((review) => {
+            return (
+              <li key={review.id} className="reviews__item">
+                <Link to={`${review.id}`}>{review.title}</Link>
+              </li>
+            );
+          })}
+      </ul>
     </div>
   );
 }
