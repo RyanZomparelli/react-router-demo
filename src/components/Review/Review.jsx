b53d273f9f5e51c633dc3ce1e2f69a1047b2e4ca
Ryan Zomparelli <zomp.ryan@gmail.com>
2025-08-12 07:45:08 -0400

    refactor(Review): adjust dynamic review matching
    - Use the id property from each review to match the URL parameter created with useParams instead of relying on array index.

--- src/components/Review/Review.jsx
+++ src/components/Review/Review.jsx
@@ -11,22 +11,18 @@ function Review({ reviews }) {
   //The useParams() hook creates an object with one property called reviewId which maps
   //to the id of the link that is clicked on.
   const { reviewId } = useParams();
-  //This way only works if the object Id's and the array indicies are sequential.
-  //Using the review.id that was clicked on we have to subract one to get the index that
-  //review is stored in because arrays are zero indexed.
-  let index = reviewId - 1;
+  const review = reviews.find((review) => review.id === parseInt(reviewId));
+
   //📌 The ? used in reviews[index]?.title is the optional chaining operator.
   // It is used here to prevent errors from occurring if the review component is
   // rendered before the API request returns with the necessary data.
   return (
     <div className="review">
-      {reviews?.[index] ? (
+      {review ? (
         <div className="review__item">
-          <h3>{reviews[index]?.title}</h3>
-          <p>{reviews[index]?.text}</p>
-          <p className="review__rating">
-            Final rating: {reviews[index]?.rating}
-          </p>
+          <h3>{review?.title}</h3>
+          <p>{review?.text}</p>
+          <p className="review__rating">Final rating: {review?.rating}</p>
           <button type="button" onClick={() => navigagateBack("/Reviews")}>
             Back to the review list 😎
           </button>
